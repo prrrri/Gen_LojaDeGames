24f1e8af8fc811b57313d74b52c5bac4dc78c6f0
Priscila Igarashi <132402519+prrrri@users.noreply.github.com>
2023-09-11 12:40:30 -0300

    atualização

---
+++ lojadegames2/src/main/java/com/generation/lojadegames2/Lojadegames2Application.java
@@ -0,0 +1,13 @@
+package com.generation.lojadegames2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lojadegames2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lojadegames2Application.class, args);
+	}
+
+}
---
+++ lojadegames2/src/test/java/com/generation/lojadegames2/Lojadegames2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.lojadegames2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lojadegames2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
